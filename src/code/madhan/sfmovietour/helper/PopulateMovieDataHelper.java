5b385472ecd70ee3a3f458666810864c5c30f1ac
Madhan Mohan <madan.mohan@lsasoftware.com>
2014-08-28 19:04:22 +0530

    Fetch Location details using Google Maps API

--- src/code/madhan/sfmovietour/helper/PopulateMovieDataHelper.java
+++ src/code/madhan/sfmovietour/helper/PopulateMovieDataHelper.java
@@ -1,18 +1,18 @@
 package code.madhan.sfmovietour.helper;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Iterator;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
 
+import code.madhan.sfmovietour.model.Location;
 import code.madhan.sfmovietour.util.AppConstants;
 
 public class PopulateMovieDataHelper {
 	
-	public double[] geocodeAddress(String address) {
+	public Location fetchLocationDetails(String address) {
 		RestTemplate client = new RestTemplate();
 		
 		String geocodeRequestUrl;
@@ -22,21 +22,44 @@ public class PopulateMovieDataHelper {
 			String geocodeResponseString = client.getForObject(geocodeRequestUrl, String.class);
 			System.out.println("Response is " + geocodeResponseString);
 			
-			ObjectMapper mapper = new ObjectMapper();
+			/*ObjectMapper mapper = new ObjectMapper();
 			JsonNode node = mapper.readValue(geocodeResponseString, JsonNode.class);
 			
 			double latitude = node.findValue("lat").asDouble();
 			double longitude = node.findValue("lng").asDouble();
-			
-			JsonNode addressArray = new ObjectMapper().readTree(geocodeResponseString).get("address_components");
-			
-			if (addressArray.isArray()) {
-				for (JsonNode arrayObj: addressArray) {
-					System.out.println("long name " + arrayObj.get("long_name").asText());
-				}
-			}
 			geocode[0] = latitude;
-			geocode[1] = longitude;
+			geocode[1] = longitude;*/
+			
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode rootNode = mapper.readTree(geocodeResponseString);
+			
+			Location location = new Location();
+			
+			Iterator<JsonNode> itr = rootNode.path("results").path(0).path("address_components").getElements();
+			
+			boolean neighborhoodFetched = false;
+			do {
+				JsonNode addressComponent = itr.next();
+				
+				for (JsonNode typeValue : addressComponent.path("types")) {
+					if (typeValue.asText().equals("neighborhood")) {
+						location.setNeighborhood(addressComponent.path("long_name").asText());
+						neighborhoodFetched = true;
+						continue;
+					}
+				}
+			} while (itr.hasNext() && !neighborhoodFetched);
+			
+			location.setLatitude(rootNode.path("results").path(0).path("geometry").path("location").path("lat").asDouble());
+			location.setLongitude(rootNode.path("results").path(0).path("geometry").path("location").path("lng").asDouble());
+			
+			
+			/*double latitude = rootNode.get("lat").asDouble();
+			System.out.println("latitude is " + latitude);
+			
+			double longitude = rootNode.get("lng").asDouble();
+			System.out.println("longitude is " + longitude);*/
+			return location;
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -44,11 +67,5 @@ public class PopulateMovieDataHelper {
 		
 		
 		return null;
-	}
-
-	public static void main(String[] args) {
-		PopulateMovieDataHelper p = new PopulateMovieDataHelper();
-			p.geocodeAddress("100 Block of Union Street, SF");
-		
 	}
 }
---
+++ src/code/madhan/sfmovietour/model/Location.java
@@ -0,0 +1,29 @@
+package code.madhan.sfmovietour.model;
+
+public class Location {
+	
+	double latitude;
+	double longitude;
+	String neighborhood;
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+	
+	
+
+}
